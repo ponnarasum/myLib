90141025f9959d989b5f895caf00f1ee261bff80
Murasu <alamelu@Murasus-Mac-mini.local>
2014-11-14 14:29:08 +0530

    Initial Commit

---
+++ myLib/myLib.h
@@ -0,0 +1,13 @@
+//
+//  myLib.h
+//  myLib
+//
+//  Created by Murasu on 14/11/14.
+//  Copyright (c) 2014 Murasu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myLib : NSObject
+
+@end
---
+++ myLib/myLib.m
@@ -0,0 +1,13 @@
+//
+//  myLib.m
+//  myLib
+//
+//  Created by Murasu on 14/11/14.
+//  Copyright (c) 2014 Murasu. All rights reserved.
+//
+
+#import "myLib.h"
+
+@implementation myLib
+
+@end
