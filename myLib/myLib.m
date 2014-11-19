0b8ed5c3706a29badebce1a8d2c42c8d618c6f06
ponnarasum <ponnarasu.m@hcl.com>
2014-11-19 13:52:27 +0530

    8th commit

--- myLib/myLib.m
+++ myLib/myLib.m
@@ -9,7 +9,7 @@
 #import "myLib.h"
 
 @implementation myLib
-- (instancetype)initWithCoder:(NSCoder *)coder
+- (instancetype) initWithCoder:(NSCoder *)coder
 {
     self = [super initWithCoder:coder];
     if (self) {
